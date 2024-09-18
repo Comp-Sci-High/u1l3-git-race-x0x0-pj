b61f74e828137dd588f191b09e7da60b01e9197f
x0x0-pj <pryah.statom26@compscihigh.org>
2024-09-18 15:19:11 +0000

    Done

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Task 1
 // Print your name
-
+Pj
 // Stage and commit this change with an appropriate message
 // Task 2
 // Print your favorite snack
-
+Swiss Roll
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
-
+Stray Kids
 // Stage and commit this change with an appropriate message
 // Go to more.js
